abb6ba20808fb5b508ea3dee885cd6d8c3d9dcb4
dzmitry <v.dmitryvasilyev@gmail.com>
2020-12-19 16:58:55 +0200

    t

--- head_page/models.py
+++ head_page/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Cars(models.Model):
+    title = models.CharField(max_length=500)
+    year_c = models.CharField(max_length=500)
+    links_c = models.CharField(max_length=500)
+    price_USE_c = models.CharField(max_length=500)
+    price_UA_c = models.CharField(max_length=500)
+    city_c = models.CharField(max_length=500)
+    #photo = models.ImageField(upload_to='photos%Y/%m/%d/', blank=True)
+
+    def __str__(self):
+        return self.title
+
+
--- selection_of_cars/settings.py
+++ selection_of_cars/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'selection_of_cars.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cars',
+        'USER': 'dzmitry',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
+
 
 
 # Password validation
@@ -120,3 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+#Для загрузки изображений
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
